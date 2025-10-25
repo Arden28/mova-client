89955592144ee4ff968ea5074683b37f80dbe983
Arden28 <laudbouetoumoussa@gmail.com>
2025-10-25 23:45:19 +0100

    Updated the map zoom

--- src/app/pages/ReservationsMap.tsx
+++ src/app/pages/ReservationsMap.tsx
@@ -128,7 +128,7 @@ export default function ReservationsMapPage() {
         container: containerRef.current,
         style: "mapbox://styles/mapbox/streets-v12",
         center: [36.8219, -1.2921],
-        zoom: 12,
+        zoom: 15,
         maxZoom: 19,
         accessToken: MAPBOX_TOKEN,
       })
