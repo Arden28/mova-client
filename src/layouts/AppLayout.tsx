1b545c5af0c0ee0b4f1e177ae31bfc2ab4b146aa
Arden28 <laudbouetoumoussa@gmail.com>
2025-10-26 21:16:27 +0100

    Updated the sidebar

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,39 +1,35 @@
+"use client"
+
 import * as React from "react"
 import { Outlet } from "react-router-dom"
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar"
+import { SidebarProvider } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 import { SiteHeader } from "@/components/site-header"
 
 import "mapbox-gl/dist/mapbox-gl.css"
 
 export default function AppLayout() {
-  const providerStyle = React.useMemo(
-    () =>
-      ({
-        "--sidebar-width": "calc(var(--spacing) * 72)", // ~288px
-        "--header-height": "calc(var(--spacing) * 12)", // ~48px
-      }) as React.CSSProperties,
-    []
-  )
-
   return (
-    <SidebarProvider style={providerStyle}>
-      <AppSidebar variant="inset" />
-      <SidebarInset>
-        <SiteHeader />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <div className="px-4 lg:px-6">
-                {/* Lazy content renders here */}
-                <React.Suspense fallback={null}>
-                  <Outlet />
-                </React.Suspense>
-              </div>
+    <SidebarProvider>
+      {/* Root flex layout: sidebar fixed at left, content takes rest */}
+      <div className="flex h-screen w-full overflow-hidden">
+        {/* Fixed sidebar */}
+        <AppSidebar />
+
+        {/* Main content area */}
+        <div className="flex flex-1 flex-col min-w-0 overflow-hidden">
+          <SiteHeader />
+
+          <main className="flex-1 overflow-auto">
+            {/* Remove old px-4/lg:px-6 to make content flush */}
+            <div className="flex flex-col flex-1">
+              <React.Suspense fallback={null}>
+                <Outlet />
+              </React.Suspense>
             </div>
-          </div>
+          </main>
         </div>
-      </SidebarInset>
+      </div>
     </SidebarProvider>
   )
 }
